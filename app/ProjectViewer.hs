5ccaf64e25844c91b7567ff692c32dfc558cdaf3
madiestic <madjestic13@gmail.com>
2024-01-09 10:08:12 +0100

    cleanup, removing src/Projects, mergin with Main

--- app/ProjectViewer.hs
+++ app/ProjectViewer.hs
@@ -7,6 +7,7 @@ import Control.Monad.Trans.Reader
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.MSF as TMSF
 import Data.MonadicStreamFunction  
+import Data.UUID (nil)
 import Foreign.C.Types  
 import Unsafe.Coerce
 import Graphics.Rendering.OpenGL as GL hiding (Select, normalize)
@@ -31,9 +32,283 @@ import Graphics.RedViz.Transformable
 import Graphics.RedViz.Widget
 import Graphics.RedViz.Uniforms
 
-import Projects.Test
-
 --import Debug.Trace as DT
+
+testProject :: Int -> Int -> Project
+testProject resx' resy' =
+  Project
+  {  
+    projname = "Test Project"
+  , resx    = resx'
+  , resy    = resy'
+  , camMode = "AbsoluteLocation"
+  , models  =
+    [ "models/pighead.gltf"
+    , "models/grid.gltf"
+    ]
+  , fontModels = sharedFonts
+  , iconModels =
+    [
+      "models/fnt_crosshair.gltf"
+    , "models/brackets.gltf"
+    ]
+  , preObjects = 
+    [ PreObject
+      {
+        pname          = "test_object"
+      , ptype          = Default
+      , pidx           = 0
+      , puuid          = nil
+      , modelIDXs      = [0,1]
+      , tsolvers       =
+        [ Identity
+        -- , Rotate
+        --   { space = ObjectSpace
+        --   , cxyz  = V3 0 0 0
+        --   , rord  = XYZ
+        --   , rxyz  = V3 0 0 (0.5)
+        --   , avel  = V3 0 0 0.05 }
+        , Translate
+          { space   = WorldSpace
+          , txyz    = V3 1.5 0 0
+          , tvel    = V3 0.0 0 0
+          , kinslv  = Identity }
+          , Rotate
+          { space   = ObjectSpace
+          , cxyz    = V3 0 0 0
+          , rord    = XYZ
+          , rxyz    = V3 0 0 (0.5)
+          , avel    = V3 0 0 (0.1)
+          , kinslv =
+            Speed
+            { life = 1.0
+            , age  = 0.0
+            , inc  = 0.01
+            , amp  = 1.0
+            , func = id }
+          }
+        -- , Translate
+        --  { space = WorldSpace
+        --  , txyz  = V3 1.1 0 0
+        --  , tvel  = V3 0.0 0 0 }
+        ]
+      , osolvers    =
+        [ Identity
+        , Select
+        ]
+        , options   = defaultBackendOptions
+        , pparent   = nil
+        , pchildren = []
+      }
+    ]
+  , preFontObject =
+    [ PreObject
+      {
+        pname      = "fonts"
+      , ptype      = Font
+      , pidx       = 0
+      , puuid      = nil
+      , modelIDXs  = [0..75]
+      , tsolvers   = []
+      , osolvers   = [ Identity ]
+      , options    = defaultBackendOptions
+      , pparent    = nil
+      , pchildren  = []
+      }
+    ]
+  , preIconObject =
+    [ PreObject
+      {
+        pname      = "crosshair"
+      , ptype      = Icon
+      , pidx       = 0
+      , puuid      = nil
+      , modelIDXs  = [0]
+      , tsolvers   = []      
+      , osolvers   = [ Identity ]
+      , options    = defaultBackendOptions
+      , pparent    = nil
+      , pchildren  = []
+      }
+    , PreObject
+      {
+        pname      = "brackets"
+      , ptype      = Icon
+      , pidx       = 1
+      , puuid      = nil
+      , modelIDXs  = [1]
+      , tsolvers   = []      
+      , osolvers   = [ Identity ]
+      , options    = defaultBackendOptions'
+      , pparent    = nil
+      , pchildren  = []
+      }
+    ]
+  , pcameras    = [ defaultCam ]
+  }
+
+parentTestProject :: Int -> Int -> Project
+parentTestProject resx' resy' =
+  Project
+  {  
+    projname = "Test Project"
+  , resx    = resx'
+  , resy    = resy'
+  , camMode = "AbsoluteLocation"
+  , models  =
+    [ "models/pighead.gltf"
+    , "models/grid.gltf"
+    ]
+  , fontModels = sharedFonts
+  , iconModels =
+    [
+      "models/fnt_crosshair.gltf"
+    , "models/brackets.gltf"
+    ]
+  , preObjects = 
+    [ PreObject
+      {
+        pname          = "pig_object"
+      , ptype          = Default
+      , pidx           = 0
+      , puuid          = nil
+      , modelIDXs      = [0]
+      , tsolvers       =
+        [ Identity
+        -- , Rotate
+        --   { space = ObjectSpace
+        --   , cxyz  = V3 0 0 0
+        --   , rord  = XYZ
+        --   , rxyz  = V3 0 0 (0.5)
+        --   , avel  = V3 0 0 0.05 }
+        , Translate
+          { space   = WorldSpace
+          , txyz    = V3 1.5 0 0
+          , tvel    = V3 0.0 0 0
+          , kinslv = Identity }
+          -- , Rotate
+          -- { space   = ObjectSpace
+          -- , cxyz    = V3 0 0 0
+          -- , rord    = XYZ
+          -- , rxyz    = V3 0 0 (0.5)
+          -- , avel    = V3 0 0 (0.1)
+          -- , kinslv  = Identity
+          --   -- Speed
+          --   -- { life = 1.0
+          --   -- , age  = 0.0
+          --   -- , inc  = 0.01
+          --   -- , amp  = 1.0
+          --   -- , func = id }
+          -- }
+        -- , Translate
+        --  { space = WorldSpace
+        --  , txyz  = V3 1.1 0 0
+        --  , tvel  = V3 0.0 0 0 }
+        ]
+      , osolvers    =
+        [ Identity
+        , Select
+        ]
+        , options   = defaultBackendOptions
+        , pparent   = nil
+        , pchildren =
+          [ PreObject
+            {
+              pname          = "grid_object"
+            , ptype          = Default
+            , pidx           = 0
+            , puuid          = nil
+            , modelIDXs      = [1]
+            , tsolvers       =
+              [ Identity
+              -- , Rotate
+              --   { space = ObjectSpace
+              --   , cxyz  = V3 0 0 0
+              --   , rord  = XYZ
+              --   , rxyz  = V3 0 0 (0.5)
+              --   , avel  = V3 0 0 0.05 }
+              , Translate
+                { space   = WorldSpace
+                , txyz    = V3 1.5 0 0
+                , tvel    = V3 0.0 0 0
+                , kinslv = Identity }
+                , Rotate
+                { space   = ObjectSpace
+                , cxyz    = V3 0 0 0
+                , rord    = XYZ
+                , rxyz    = V3 0 0 (0.5)
+                , avel    = V3 0 0 (0.02)
+                , kinslv  = Identity
+                  -- Speed
+                  -- { life = 1.0
+                  -- , age  = 0.0
+                  -- , inc  = 0.01
+                  -- , amp  = 1.0
+                  -- , func = id }
+                }
+              -- , Translate
+              --  { space = WorldSpace
+              --  , txyz  = V3 1.1 0 0
+              --  , tvel  = V3 0.0 0 0 }
+                , Parent
+              ]
+            , osolvers  =
+              [ Identity
+              , Select
+              ]
+              , options   = defaultBackendOptions
+              , pparent   = nil
+              , pchildren = []
+            }            
+          ]
+      }      
+    ]
+  , preFontObject =
+    [ PreObject
+      {
+        pname      = "fonts"
+      , ptype      = Font
+      , pidx       = 0
+      , puuid      = nil
+      , modelIDXs  = [0..75]
+      , tsolvers   = []      
+      , osolvers   = [ Identity ]
+      , options    = defaultBackendOptions
+      , pparent    = nil
+      , pchildren  = []
+      }
+    ]
+  , preIconObject =
+    [ PreObject
+      {
+        pname      = "crosshair"
+      , ptype      = Icon
+      , pidx       = 0
+      , puuid      = nil
+      , modelIDXs  = [0]
+      , tsolvers   = []            
+      , osolvers   = [ Identity ]
+      , options    = defaultBackendOptions
+      , pparent    = nil
+      , pchildren  = []
+      }
+    , PreObject
+      {
+        pname      = "brackets"
+      , ptype      = Icon
+      , pidx       = 1
+      , puuid      = nil
+      , modelIDXs  = [1]
+      , tsolvers   = []            
+      , osolvers   = [ Identity ]
+      , options    = defaultBackendOptions'
+      , pparent    = nil
+      , pchildren  = []
+      }
+    ]
+  , pcameras    = [ defaultCam ]
+  }
+
 
 gameLoop :: MSF (MaybeT (ReaderT GameSettings (ReaderT Double (StateT Game IO)))) () Bool
 gameLoop = runGame `untilMaybe` gameQuit `catchMaybe` exit
